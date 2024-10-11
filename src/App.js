f4e0489db7c9b65ab80c211f036a57ae708aaa89
Rita B <margarita.bern@gmail.com>
2024-10-11 16:04:00 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">  
+      <div className='container'>
     <h1>Weather App
       </h1> 
     <footer> This project was coded by Rita Bern and is
    
     <a href="https://github.com/finnocino/react-weather-project" target='_blank' rel="noreferrer"> open-sourced on Github </a>
      </footer>
+     </div>
     </div>
   );
 }
