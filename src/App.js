bc735b90a7055ea19839ef2d827fda5a356f56d8
Rita B <margarita.bern@gmail.com>
2024-10-11 01:28:48 -0400

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">   Weather App
+    <div className="App">  
+    <h1>Weather App
+      </h1> 
+    <footer> This project was coded by Rita Bern and is
+   
+    <a href="https://github.com/finnocino/react-weather-project" target='_blank'> open-sourced on Github </a>
+     </footer>
     </div>
   );
 }
