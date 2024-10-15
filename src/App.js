342d3096726a7a7860848d138fa6109cd278c34a
Rita B <margarita.bern@gmail.com>
2024-10-14 20:51:29 -0400

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ export default function App() {
   return (
     <div className="App">  
       <div className='container'>
-    <h1>
-      Weather App
-      </h1> 
-
-    <Weather />
+    
+    <Weather defaultCity = "New York" />
     <footer> This project was coded by Rita Bern and is
-    <a href="https://github.com/finnocino/react-weather-project" target='_blank' rel="noreferrer"> open-sourced on Github </a>
+    <a href="https://github.com/finnocino/react-weather-project" 
+    target='_blank' 
+    rel="noreferrer"> open-sourced on Github </a>
      </footer>
      </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios";
 
-export default function Weather() {
-    return (
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            description: response.data.weather[0].description,
+            date: "Wednesday 5:30",
+        });
+        
+    }
+
+    if (weatherData.ready) {
+          return (
     <div className="Weather">
         <form>
             <div className="row">
@@ -18,27 +36,37 @@ export default function Weather() {
                 </div>
             </div>
         </form>
-        <h1>Toronto</h1>
+        <h1>{weatherData.city}</h1>
          <ul>
-            <li>Friday 4:55</li>
-            <li>Mostly Cloudy</li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                 alt="Cloudy"
+                <img src = {weatherData.iconUrl}
+                 alt={weatherData.description}
                  />
-               <span className="temperature">18</span>
+               <span className="temperature">{Math.round(weatherData.temperature)}</span>
                  <span className="unit">°C</span>  
                </div>
            <div className="col-6">
               <ul>
                 <li>Precipitation: 40%</li>
-                <li>Humidity: 50%</li>
-                <li>Wind: 10 km/h</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}</li>
              </ul>
            </div>
        </div>
     </div>
-    )
+    ); 
+} else {
+  const apiKey = "c6f8ef4575250284954db9f4dfa7a996";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&&units=metric`;
+    /*`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;*/
+    
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..."
 }
+   
+   }
