3c571542056e9c32a7fab51c2afa7fcdfc9d2a6c
Rita B <margarita.bern@gmail.com>
2024-10-24 17:58:12 -0400

    Added useEffect
    Added useEffect to Weather Forecast.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props) {
           <div className="col-6">
             <div className="d-flex">
                      <div> 
-                    <WeatherIcon code={props.data.icon} size={52}  />        
+                    <WeatherIcon code={props.data.icon} size={56}  />        
                     </div>
                     <div> 
                      <WeatherTemperature celsius={props.data.temperature} />    
